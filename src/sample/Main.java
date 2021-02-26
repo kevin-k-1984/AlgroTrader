d0cb1ebfab7aa7e8685d4302e227c91a1a81a439
kevin_k <kkemp1984@gmail.com>
2021-02-26 11:41:28 -0600

    testing commits

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,10 +19,9 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-
-
+        int x = 5;
+        int y = 10;
+        int z = x + y;
         launch(args);
-
-
     }
 }
